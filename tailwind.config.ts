138ade74585d831574bf24236ff01811ca028c46
lnrdgnwn <leonardogunawan15@gmail.com>
2024-12-07 12:00:00 +0700

    Update config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,78 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        "whtc-100": "hsl(var(--whtc-100))",
+        "whtc-200": "hsl(var(--whtc-200))",
+        "shadow-100": "hsl(var(--shadow-100))",
+        "shadow-200": "hsl(var(--shadow-200))",
+        "shadow-300": "rgb(var(--shadow-300))",
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        "text-100": "hsl(var(--text-100))",
+        "text-200": "hsl(var(--text-200))",
+        "text-300": "hsl(var(--text-300))",
+        "primary-100": "hsl(var(--primary-100))",
+        "primary-200": "hsl(var(--primary-200))",
+        "primary-300": "hsl(var(--primary-300))",
+        "black-100": "rgb(var(--black-100))",
+        "black-200": "rgb(var(--black-200))",
+        "black-300": "hsl(var(--black-300))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+      fontFamily: {
+        monument: "var(--font-monument)",
+        inter: ["Inter"],
+        ponnala: "var(--font-ponnala)",
+        poppins: "var(--font-poppins)",
       },
     },
   },
